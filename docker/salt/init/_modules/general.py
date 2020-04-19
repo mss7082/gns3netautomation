e1663bb995eaf01b39bbf09faa0cbc8ec6c3c596
Moses Sokabi <mssokabi@pop-os.localdomain>
2020-04-19 23:11:41 +0100

    Renaming module

--- docker/salt/init/_modules/general.py
+++ docker/salt/init/_modules/general.py
@@ -1,18 +1,6 @@
 import re
 
 
-def first():
-    return True
-
-
-def second():
-    return __salt__["test.false"]()
-
-
-def third():
-    return __salt__["sample.first"]()
-
-
 def users():
     users = []
     pattern = re.compile(r"user\s[\w]+")
