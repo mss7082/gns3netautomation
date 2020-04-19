bc6219ae1290783036d14ac56ab269a34826d010
Moses Sokabi <mssokabi@pop-os.localdomain>
2020-04-19 22:59:07 +0100

    exploring regex

--- docker/salt/init/_modules/sample.py
+++ docker/salt/init/_modules/sample.py
@@ -14,7 +14,11 @@ def third():
 
 
 def users():
+    users = []
     pattern = re.compile(r"user\s[\w]+")
     res = __salt__["napalm.netmiko_commands"](
         "show configuration system login")
-    return pattern.findall(res)
+    matches = pattern.findall(res[0])
+    for match in matches:
+        users.append(match.split(" ")[1])
+    return users
