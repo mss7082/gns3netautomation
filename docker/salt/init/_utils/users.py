f9af0e35fe383f15ca4aa1f4d7a4e10f4911d705
Moses Sokabi <mssokabi@pop-os.localdomain>
2020-04-22 17:14:00 +0100

    Modularize using util custom modules

--- docker/salt/init/_utils/users.py
+++ docker/salt/init/_utils/users.py
@@ -1,6 +1,13 @@
 import re
 
 
+def __virtual__():
+    if __grains__["os"] == "junos":
+        return "general"
+    else:
+        return (False, 'Not loading this module, as this is not a Junos device')
+
+
 def get_users():
     '''
     Returns a list of users configured on network devices.
@@ -16,17 +23,19 @@ def get_users():
     users = []
     pattern = re.compile(r"username\s[\w]+")
     if __grains__["os"] == "ios":
-        res = __salt__["napalm.netmiko_commands"](
-            "show run | i  username")
+        res = __salt__["napalm.netmiko_commands"]("show run | i  username")
     elif __grains__["os"] == "junos":
         res = __salt__["napalm.netmiko_commands"](
             "show configuration system login")
     elif __grains__["os"] == "iosxr":
-        res = __salt__["napalm.netmiko_commands"](
-            "show run username")
+        res = __salt__["napalm.netmiko_commands"]("show run username")
     else:
         return (False, "This function is not supported yet on this platform")
     matches = pattern.findall(res[0])
     for match in matches:
         users.append(match.split(" ")[1])
     return users
+
+
+./namecheap_ssl.py: import salt.utils.namecheap
+./napalm_snmp.py: import salt.utils.napalm
